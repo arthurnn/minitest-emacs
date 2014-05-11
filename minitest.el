d8ed7ff381988b1b1ecb82bd1c78d1bba16d3f20
Arthur Neves <arthurnn@gmail.com>
2014-05-11 12:43:53 -0400

    prepare 0.7.1

--- minitest.el
+++ minitest.el
@@ -4,7 +4,7 @@
 
 ;; Author: Arthur Neves
 ;; URL: https://github.com/arthurnn/minitest-emacs
-;; Version: 0.7
+;; Version: 0.7.1
 ;; Package-Requires: ((dash "1.0.0"))
 
 ;; This file is NOT part of GNU Emacs.
